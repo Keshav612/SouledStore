9c44d430d3ffa3f8875775df04e9617d455d5a85
Keshav612 <500062098@stu.upes.ac.in>
2020-03-11 14:36:59 +0530

    @test to check if sizing exist in that page

--- src/souled/souled.java
+++ src/souled/souled.java
@@ -162,6 +162,19 @@ public class souled
 			 System.out.println("Element not present");
 		}
 	 }
+	 @Test (priority=12)
+	 void sizing()
+	 {
+		 try
+		 {
+			 driver.findElements(By.xpath("//*[@id=\"collapseOne\"]/div/p/div/b[2]"));//xpath("/html/body/div[1]/div/div[3]/div[2]/div/div[2]/div[2]/div/div[7]/div/div[2]/div/span[1]/i")
+			 System.out.println("Element present");
+		}
+			catch(NoSuchElementException e){
+			
+			 System.out.println("Element not present");
+		}
+	 }
 }
 	 
 	
